4ae09d36a027988ce0a037a771f42c828188fb52
Maria <103815224+dymshamaria@users.noreply.github.com>
2022-06-15 16:59:07 +0300

    Update mariadymsha. HW1.js

--- mariadymsha. HW1.js
+++ mariadymsha. HW1.js
@@ -17,18 +17,23 @@ item_5 = item_3
 let item_6
 let item_6_type
 item_6 = 15
-item_6_type = item_6
-console.log( "item_6 ==" + item_6, "item_6_type ==", item_6_type )
+item_6_type = typeof(item_6)
+console.log( "item_6 ==" + typeof(item_6), "item_6_type ==" + typeof(item_6_type ))
 let item_7 = String(item_6)
 let item_7_type
-item_7_type = item_7
-console.log( "item_7 ==" + item_7, "item_7_type ==", item_7_type )
+item_7_type =typeof(item_7)
+console.log( "item_7 ==" + typeof(item_7), "item_7_type ==" + typeof(item_7_type ))
 let age_1 = 10
 let age_2 = 18
 let age_3 = 60
-if (age_1){
-    console.log("You don't have access cause your age is" + age_1 + "It's less then" + age_2 , age_1 < age_2)
-    console.log("Welcome !", age_1 >= age_2, age_1 < age_3)
-    console.log("Keep calpm and look Culture channel", age_1> age_3)
-}    else { console.log("Technical work")
+if (age_1 < age_2){
+    console.log("You don't have access cause your age is" + age_1 + "." + "It's less then" + age_2 + ".")
+} else if (age_1 >= age_2 & age_1 < age_3){
+    console.log("Welcome !")
+    }
+    else if (age_1> age_3) {
+    console.log("Keep calpm and look Culture channel")
+    } else { 
+        console.log("Technical work")
 }
+    
